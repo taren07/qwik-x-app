e8161f2a6445b3d6b713da06534b8ef873121ee1
taren07 <taro.enchi@gmail.com>
2024-03-05 23:38:28 +0900

    update main.go

--- backend/app/main.go
+++ backend/app/main.go
@@ -16,6 +16,9 @@ func main() {
 	userUsecase := usecase.NewUserUsecase(userRepository, userValidator)
 	userController := controller.NewUserController(userUsecase)
 
-	e := router.NewRouter(userController)
+	commentRepository := repository.NewCommentRepository(db)
+	commentUsecase := usecase.NewCommentUsecase(commentRepository)
+	commentController := controller.NewCommentController(commentUsecase)
+	e := router.NewRouter(userController, commentController)
 	e.Logger.Fatal(e.Start(":8080"))
 }
