225413280eb0ce762e270b659917335ff65706ca
taren07 <taro.enchi@gmail.com>
2024-03-20 22:34:30 +0900

    add discussion header focus

--- frontend/src/routes/post/post.css.ts
+++ frontend/src/routes/post/post.css.ts
@@ -1,4 +1,12 @@
-import { style } from "@vanilla-extract/css";
+import { createGlobalTheme, style } from "@vanilla-extract/css";
+
+export const vars = createGlobalTheme(":root", {
+	color: {
+		grayLight: "#f0f0f0",
+		grayBorders: "1px solid #d0d0d0",
+		primary: "#007bff",
+	},
+});
 
 export const discussion = style({
 	borderRadius: "0.5rem",
@@ -17,4 +25,25 @@ export const container = style({
 	height: "100vh",
 	width: "100vw",
 	backgroundColor: "#ECF0F4",
+});
+
+export const discussionHeader = style({
+	backgroundColor: vars.color.grayLight,
+	borderBottom: vars.color.grayBorders,
+	display: "flex",
+	gap: "0.5rem",
+	borderRadius: "0.5rem 0.5rem 0 0",
+	overflow: "hidden",
+	padding: "1rem",
+	width: "100%",
+});
+
+export const discussionHeaderTextareaFocus = style({
+	selectors: {
+		"&:focus": {
+			outline: "none",
+			height: "5rem",
+			border: `1px solid ${vars.color.primary}`,
+		},
+	},
 });
